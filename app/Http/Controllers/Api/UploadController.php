811b8647f122b6f3c3520cd56159ba26fcfd9d7f
Ridwan Lawson <ridwanlawsons@gmail.com>
2026-09-01 13:39:47 +0700

    fix gang code actual

--- app/Http/Controllers/Api/UploadController.php
+++ app/Http/Controllers/Api/UploadController.php
@@ -1497,7 +1497,7 @@ class UploadController extends Controller
                             BASIS_BM, KG_JANJANG, BJR, DOCUMENTNO, SUPERVISION_5
                         )
                         SELECT
-                            ld.GANGCODE,
+                            IPLASPROD.EMPLOYEE_API.GET_GANGCODE(ld.FCBA, ld.EMPLOYEECODE),
                             FDDATE,
                             SUPERVISION_1,
                             SUPERVISION_2,
